2c03f65f98e863c646cc591f2b215549a20e45e9
mouhsine82 <mboukhris6148@gmail.com>
2023-04-10 18:00:34 -0700

    Furniture project ready

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      Table table=new Table();
+      table.brand="Ikea";
+      table.weight="60";
+      table.price="250";
+    }
+}
---
+++ src/Table.java
@@ -0,0 +1,5 @@
+public class Table {
+    String brand;
+    String weight;
+    String price;
+}
